b23c428f6890ec935666bbd6f981b8cf9242d356
user1gh1 <user1gm1@gmail.com>
2025-12-04 19:38:05 +0400

    ebs_driver provider-commented

--- cluster/aws/main.tf
+++ cluster/aws/main.tf
@@ -129,31 +129,31 @@ module "cni_policy" {
 
 #======== EBS DRIVER ========
 
-module "ebs_driver" {
-  source = "git::https://github.com/devops-tasks-lab/k8s-accelerator-modules.git//helm_release/helm?ref=version-1"
-  count  = var.ebs_driver_count
+# module "ebs_driver" {
+#   source = "git::https://github.com/devops-tasks-lab/k8s-accelerator-modules.git//helm_release/helm?ref=version-1"
+#   count  = var.ebs_driver_count
 
-  # providers = {
-  #   helm = helm.eks
-  # }
+#   # providers = {
+#   #   helm = helm.eks
+#   # }
 
-  application_name             = var.ebs_driver_name
-  application_namespace        = var.ebs_driver_namespace
-  application_chart            = var.ebs_driver_chart
-  application_version          = var.ebs_driver_version
-  application_repository       = var.ebs_driver_repository
-  application_create_namespace = var.ebs_driver_create_namespace
-  #application_values           = var.helm_values_ebs
-  application_values = [
-    <<-EOT
-  controller:
-    serviceAccount:
-      name: ebs-csi-controller-sa
-      create: true
-      annotations:
-        eks.amazonaws.com/role-arn: ${module.oidec_token.ebs_csi_role_arn}
-  EOT
-  ]
+#   application_name             = var.ebs_driver_name
+#   application_namespace        = var.ebs_driver_namespace
+#   application_chart            = var.ebs_driver_chart
+#   application_version          = var.ebs_driver_version
+#   application_repository       = var.ebs_driver_repository
+#   application_create_namespace = var.ebs_driver_create_namespace
+#   #application_values           = var.helm_values_ebs
+#   application_values = [
+#     <<-EOT
+#   controller:
+#     serviceAccount:
+#       name: ebs-csi-controller-sa
+#       create: true
+#       annotations:
+#         eks.amazonaws.com/role-arn: ${module.oidec_token.ebs_csi_role_arn}
+#   EOT
+#   ]
 
-  depends_on = [module.node-group]
-}
+#   depends_on = [module.node-group]
+# }
