c4c01d918f6b50c438d88bd65e18aca0666fccf6
aarakelyan-sorenson <AArakelyan@sorenson.com>
2025-12-11 12:52:26 +0400

    yandex providers

--- cluster/yandex/providers.tf
+++ cluster/yandex/providers.tf
@@ -1,17 +1,17 @@
-# terraform {
-#   required_providers {
-#     yandex = {
-#       source  = "yandex-cloud/yandex"
-#       version = ">= 0.120.0"
-#     }
-#   }
-#   required_version = ">= 1.1.0"
-# }
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.120.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
 
 
-# provider "yandex" {
-#   cloud_id                 = var.yc_cloud_id
-#   folder_id                = var.yc_folder_id
-#   service_account_key_file = var.yc_service_account_key_file
-#   zone                     = var.zone
-# }
+provider "yandex" {
+  cloud_id                 = var.yc_cloud_id
+  folder_id                = var.yc_folder_id
+  service_account_key_file = var.yc_service_account_key_file
+  zone                     = var.zone
+}
