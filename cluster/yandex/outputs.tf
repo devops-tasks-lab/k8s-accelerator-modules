af47d86d5dccf8d06b1bb49a9bd2fdf708b450af
aarakelyan-sorenson <AArakelyan@sorenson.com>
2025-12-11 17:45:06 +0400

    add output for yandex-helm-connectiong

--- cluster/yandex/outputs.tf
+++ cluster/yandex/outputs.tf
@@ -1,3 +1,14 @@
+# output "cluster_id" {
+#   value = yandex_kubernetes_cluster.cluster.id
+# }
 output "cluster_id" {
-  value = yandex_kubernetes_cluster.cluster.id
+  value = yandex_kubernetes_cluster.primary.id
+}
+
+output "cluster_endpoint" {
+  value = yandex_kubernetes_cluster.primary.master[0].external_v4_endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = yandex_kubernetes_cluster.primary.master[0].cluster_ca_certificate
 }
